9162a9031cec7ec002a71ccb90bcdd60459d5572
gichimux <gichimumwai1@gmail.com>
2019-06-03 17:34:42 +0300

    imported application factory

---
+++ manage.py
@@ -0,0 +1,29 @@
+from app import create_app, db
+from app.models import User, Role, Post, Follow, Permission, Admin
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('development')
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('server', Server)
+manager.add_command('db', MigrateCommand)
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role, Post=Post, \
+                Follow=Follow, Permission=Permission, Admin=Admin)
+
+@manager.command
+def test():
+    '''
+    Run the unit tests.
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+
+if __name__ == '__main__':
+    manager.run()
