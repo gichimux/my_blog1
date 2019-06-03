f5219b77f6d78a80f69dde136bc18db76b2074a6
gichimux <gichimumwai1@gmail.com>
2019-06-03 17:36:03 +0300

    initialized application blueprints

---
+++ app/__init__.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_babel import Babel
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_pagedown import PageDown
+from flask_moment import Moment
+from flask_whooshee import Whooshee
+from config import config_options
+
+babel = Babel()
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+
+pagedown = PageDown()
+moment = Moment()
+mail = Mail()
+whooshee = Whooshee()
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config_options[config_name])
+    # config[config_name].init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    pagedown.init_app(app)
+    whooshee.init_app(app)
+
+    if not app.debug and not app.testing and not app.config['SSL_DISABLE']:
+        from flask_sslify import SSLify
+        sslify = SSLify(app)
+
+    from .user import user as user_blueprint
+    from .main import main as main_blueprint
+    from .admin import admin as admin_blueprint
+    from .message import message as message_blueprint
+    from .api import api as api_blueprint
+
+    app.register_blueprint(user_blueprint)
+    app.register_blueprint(main_blueprint, url_prefix='/main')
+    app.register_blueprint(admin_blueprint, url_prefix='/admin')
+    app.register_blueprint(message_blueprint, url_prefix='/message')
+    app.register_blueprint(api_blueprint, url_prefix='/api')
+
+    return app
